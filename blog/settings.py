63e33fdd84328cd51e0023b49ba634704b1581af
bundelabun <audreyfu14@gmail.com>
2021-03-30 09:19:52 -0700

    debugging sass integration

--- blog/settings.py
+++ blog/settings.py
@@ -122,11 +122,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
 
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/'),
+]
 
 SASS_PROCESSOR_ROOT = STATIC_ROOT
 
@@ -145,10 +148,6 @@ BOOTSTRAP3 = {
 }
 
 STATIC_SOURCE_ROOT = os.path.join(BASE_DIR, 'static/')
-
-STATICFILES_DIRS = [
-    STATIC_SOURCE_ROOT
-]
 
 SASS_PROCESSOR_INCLUDE_DIRS = [
     os.path.join(STATIC_SOURCE_ROOT, 'scss/')
